8be18d9099c4192928e4c3b617a23dc4e67f49a7
Steve De La Rosa <stevedelarosa1036@gmail.com>
2025-05-02 12:52:19 -0400

    set up typescript files and ran server

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,15 +1,14 @@
+"use strict";
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const app = express();
 const cors = require("cors");
 const knex = require("knex");
 const morgan = require("morgan");
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan("combined"));
 app.use(cors());
-
 app.listen(3000, () => {
     console.log("App is running on port 3000");
 });
