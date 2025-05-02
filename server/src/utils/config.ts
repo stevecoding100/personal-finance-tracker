f9b56007abb17af678096a64e4655baf3facc085
Steve De La Rosa <stevedelarosa1036@gmail.com>
2025-05-02 13:14:15 -0400

    added more remaining files to start project

---
+++ server/src/utils/config.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const config = {
+    app: {
+        port: process.env.PORT || 5000,
+        nodeEnv: process.env.NODE_ENV || "development",
+    },
+    db: {
+        host: process.env.DB_HOST!,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER!,
+        password: process.env.DB_PASSWORD!,
+        database: process.env.DB_NAME!,
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET!,
+        expiresIn: "7d",
+    },
+};
